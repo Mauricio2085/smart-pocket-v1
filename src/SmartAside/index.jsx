769027ba36a10e71ec42e90392236141f3ce0d01
Mauricio Ocampo <maoca2085@gmail.com>
2023-06-24 23:10:46 -0500

    maquetacion y configuracion de margenes del aside

--- src/SmartAside/index.jsx
+++ src/SmartAside/index.jsx
@@ -3,8 +3,14 @@ import './SmartAside.css'
 
 function SmartAside() {
     return(
-        <aside className=" w-1/4 ml-2 mt-2 mb-2 rounded-lg bg-gradient-to-r from-cyan-500 to-blue-500 drop-shadow-md">
-            <p>Test</p>
+        <aside className="flex-col w-[26%] ml-2 mt-2 mb-3 rounded-lg">
+            <section className="h-1/4 mb-1 rounded-lg ">
+                <div className="h-16 bg-sky-300 border-b border-indigo-400 rounded-t-lg"></div>
+                <div className="h-16 bg-sky-300 rounded-b-lg"></div>
+            </section>
+            <section className="h-3/4 rounded-lg bg-sky-300">
+                
+            </section>
         </aside>
     );
 }
