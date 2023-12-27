ae46c153a0262445f25816ed331fb56002795915
Mauricio2085 <maoca@gmail.com>
2023-12-27 16:00:50 -0500

    Header with responsive design finished

--- src/SmartHeader/index.jsx
+++ src/SmartHeader/index.jsx
@@ -2,28 +2,37 @@ import React from "react"
 import { SmartIcons } from "../SmartIcon"
 import { FaShoppingCart } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
-
+import { GiHamburgerMenu } from "react-icons/gi";
+import { VscAccount } from "react-icons/vsc";
 
 
 function SmartHeader() {
     return (
-        <header className="w-full border-b border-white bg-gradient-to-r from-cyan-500 to-blue-500">
-        <nav className=" mx-36 py-6 flex flex-row justify-between items-center">
-            <div className=" w-44 inline content-start"><SmartIcons /></div>
-                <div className=" w-[500px] h-8 flex flex-row justify-center items-center bg-slate-200 rounded-lg" >
-                    <div className="w-8 h-8 flex justify-center items-center">
-                    <FaSearch size={20} fill="#7dd3fc"/>
+        <header className=" z-10 w-full border-b border-white bg-gradient-to-r from-cyan-500 to-blue-500">
+        <nav className=" mx-5 sm:mx-5 xl:mx-36 py-6 xl:py-6 flex flex-col sm:flex sm:flex-col xl:flex xl:flex-col xl:flex-nowrap justify-between xl:justify-between items-start xl:items-center">
+            <section className=" w-full md:w-full flex flex-row flex-nowrap md:flex md:flex-row md:justify-between justify-between items-center">
+                    <div className=" w-10 h-full md:hidden flex justify-center items-center md:justify-center md:items-center">
+                        <GiHamburgerMenu size={28} fill="#7dd3fc"/>
                     </div>
-                    <input type="text" placeholder="Encuentra tu producto favorito!" className=" bg-slate-200 w-full px-1 font-DynaPuff text-sm "/>
-                </div>
-                <div className="">
+                    <SmartIcons />
+                    <div className="">
                     <ul className="flex justify-center items-center">
                         <li className="">
-                            <button className=" w-28 h-10 mr-10 border border-blue-800 rounded-lg font-DynaPuff text-sm">Iniciar sesión</button>
+                            <button className=" hidden md:block md:w-28 md:h-10 md:mr-8 md:border md:border-blue-800 md:rounded-lg font-DynaPuff text-sm ">Iniciar sesión</button>
+                            <button className=" md:hidden w-10 h-full pr-2 flex justify-center flex-wrap ">
+                                <VscAccount size={28} fill="#7dd3fc"/>
+                            </button>
                         </li>
-                        <li className=" w-10 h-full" alt="Administrador" ><FaShoppingCart size={24} fill="#7dd3fc"/></li>
+                        <button className=" w-10 h-full flex justify-center flex-wrap" alt="Carrito de compras" ><FaShoppingCart size={28} fill="#7dd3fc"/></button>
                     </ul>
+                </div>    
+            </section>
+            <div className=" w-full mt-5 md:mt-5 md:mr-10 lg:mr-10 md:w-[320px] lg:w-[500px] h-8 flex flex-row justify-center items-center bg-slate-200 rounded-lg md:absolute lg:absolute md:top-5 lg:top-5 sm:self-center " >
+                <div className="w-8 h-8 flex justify-center items-center focus:outline-dotted">
+                    <FaSearch size={20} fill="#7dd3fc"/>
                 </div>
+                <input type="text" placeholder="Encuentra tu producto favorito!" className=" bg-slate-200 w-full h-full px-1 font-DynaPuff text-sm rounded-lg focus:outline-none "/>
+            </div>    
         </nav>
     </header>
     )
