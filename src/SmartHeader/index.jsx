f06e41401348862a56c05ba0460f263fcad1daf7
Mauricio2085 <maoca2085@gmail.com>
2025-01-21 19:44:46 -0500

    Se finaliza diseño mobile first del header, pendiente crear menu

--- src/SmartHeader/index.jsx
+++ src/SmartHeader/index.jsx
@@ -7,33 +7,31 @@ import { VscAccount } from "react-icons/vsc";
 
 const SmartHeader = () => {
 	return (
-		<header className=" z-10 md:w-full sm:w-full xl:w-full lg:w-full border-b border-white bg-gradient-to-r from-cyan-500 to-blue-500">
-			<nav className=" mx-5 sm:mx-5 xl:mx-36 py-6 xl:py-6 flex flex-col sm:flex sm:flex-col xl:flex xl:flex-col xl:flex-nowrap justify-between xl:justify-between items-start xl:items-center">
-				<section className=" w-full md:w-full flex flex-row flex-nowrap md:flex md:flex-row md:justify-between justify-between items-center">
-					<div className=" w-10 h-full md:hidden flex justify-center items-center md:justify-center md:items-center">
+		<header className="w-full border-b border-white bg-gradient-to-r from-cyan-500 to-blue-500">
+			<nav className=" px-5 xl:px-36 py-5 flex flex-col xl:flex-flex-nowrap xl:justify-between ">
+				<section className=" flex flex-row flex-nowrap md:flex md:flex-row md:justify-between justify-between items-center">
+					<div className=" w-10 h-full xl:hidden flex justify-center items-center md:justify-center md:items-center">
 						<GiHamburgerMenu size={28} fill="#7dd3fc" />
 					</div>
 					<SmartIcons />
-					<div className="">
-						<ul className="flex justify-center items-center">
-							<li className="">
-								<button className=" hidden md:block md:w-28 md:h-10 md:mr-8 md:border md:border-blue-800 md:rounded-lg font-DynaPuff text-sm ">
-									Iniciar sesión
-								</button>
-								<button className=" md:hidden w-10 h-full pr-2 flex justify-center flex-wrap ">
-									<VscAccount size={28} fill="#7dd3fc" />
-								</button>
-							</li>
-							<button
-								className=" w-10 h-full flex justify-center flex-wrap"
-								alt="Carrito de compras"
-							>
-								<FaShoppingCart size={28} fill="#7dd3fc" />
+					<ul className="flex justify-center items-center">
+						<li className="">
+							<button className=" hidden xl:block xl:w-28 xl:h-10 xl:mr-8 xl:border xl:border-blue-800 xl:rounded-lg font-DynaPuff text-sm ">
+								Iniciar sesión
 							</button>
-						</ul>
-					</div>
+							<button className=" xl:hidden w-10 h-full pr-2 flex justify-center flex-wrap ">
+								<VscAccount size={28} fill="#7dd3fc" />
+							</button>
+						</li>
+						<button
+							className=" hidden w-10 h-full justify-center flex-wrap"
+							alt="Carrito de compras"
+						>
+							<FaShoppingCart size={28} fill="#7dd3fc" />
+						</button>
+					</ul>
 				</section>
-				<div className=" w-full mt-5 md:mt-5 md:mr-10 lg:mr-10 md:w-[320px] lg:w-[500px] h-8 flex flex-row justify-center items-center bg-slate-200 rounded-lg md:absolute lg:absolute md:top-5 lg:top-5 sm:self-center ">
+				<div className=" w-full my-5 flex flex-row justify-center items-center h-8 bg-slate-200 rounded-lg xl:w-[500px] xl:absolute xl:top-5 xl:self-center">
 					<div className="w-8 h-8 flex justify-center items-center focus:outline-dotted">
 						<FaSearch size={20} fill="#7dd3fc" />
 					</div>
--- src/SmartPublic/index.jsx
+++ src/SmartPublic/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function SmartPublic(props) {
 	return (
-		<section className=" lg:mx-36 lg:my-16 lg:grid lg:grid-cols-3 lg:gap-2 md:mx-36 md:my-16 md:grid md:grid-cols-3 md:gap-2 flex flex-col justify-center">
+		<section className=" my-5 flex flex-col justify-center lg:mx-36 lg:my-16 lg:grid lg:grid-cols-3 lg:gap-2 md:my-16 md:grid md:grid-cols-2 md:gap-2 ">
 			{props.children}
 		</section>
 	);
