a7a7edab3b8d6f295beea69db2926c185a48e857
Mauricio2085 <maoca2085@gmail.com>
2025-02-01 23:22:24 -0500

    Se actualizan rutas en CategoryPage y ProductDetailPage, se reduce el tiempo de espera en useFetch, se añade componente ProductCard y se realizan mejoras en el diseño de varios componentes.

--- src/Pages/ProductDetailPage.js
+++ src/Pages/ProductDetailPage.js
@@ -1,12 +1,46 @@
-import React from "react";
 import { useParams } from "react-router-dom";
+import { ProductCard } from "../components/ProductCard";
+import { Loading } from "../components/Loading";
+import { useFetch } from "../Hooks/useFetch";
+import { useNavigate } from "react-router-dom";
 
 const ProductDetailPage = () => {
-	const { productName, productId } = useParams();
+	const { categoryName, productId } = useParams();
+	const { data, loading, error } = useFetch(
+		`http://localhost:5000/api/v1/productos/product-detail/${categoryName}/${productId}`
+	);
+	let navigate = useNavigate();
+	if (!!loading) return <Loading />;
+	if (!!error) return <h1>Error</h1>;
+
 	return (
 		<>
-			<h1>Nombre Producto: {productName}</h1>
-			<h1>Id Producto: {productId}</h1>
+			<section className=" w-full h-10 px-5 md:px-20 lg:px-36 flex flex-col justify-center items-start font-DynaPuff text-sm m-auto bg-blue-700">
+				<button
+					onClick={() =>
+						navigate(
+							`../../categorias/${categoryName}/${data.product[0].categoria_id}`
+						)
+					}
+					className="w-20 h-8 rounded-md bg-cyan-500 border border-yellow-100"
+				>
+					Atrás
+				</button>
+			</section>
+			<section className=" w-full px-5 md:px-20 lg:px-36 flex ">
+				<div className=" w-full flex flex-col items-center">
+					<section className="w-full bg-white md:my-8 ">
+						<ProductCard
+							key={data.product[0].id_producto}
+							imagenProducto={data.product[0].imagen_producto}
+							nombreProducto={data.product[0].nombre_producto}
+							precioOferta={data.product[0].precio_venta}
+							precioVenta={data.product[0].precio_venta}
+							descripcionProducto={data.product[0].descripcion}
+						/>
+					</section>
+				</div>
+			</section>
 		</>
 	);
 };
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+const ProductCard = ({
+	idProducto,
+	nombreProducto,
+	imagenProducto,
+	precioVenta,
+	precioOferta,
+	descripcionProducto,
+}) => {
+	const formattedAmount = (amount) => {
+		return new Intl.NumberFormat("es-CO", {
+			style: "currency",
+			currency: "COP",
+			minimumFractionDigits: 0,
+			maximumFractionDigits: 0,
+		}).format(amount);
+	};
+
+	return (
+		<>
+			<div key={idProducto} className=" my-8 flex-col bg-white xl:px-60 ">
+				<div className="w-full rounded-md flex flex-col sm:grid sm:grid-cols-2 sm:gap-4 border border-slate-100 drop-shadow-md">
+					<div className="w-full flex justify-center sm:order-1 sm:row-span-3 ">
+						<img
+							className="w-full h-auto max-w-[300px] object-contain aspect-square rounded-lg"
+							src={imagenProducto}
+							alt="Imagen"
+						/>
+					</div>
+					<div className="w-full flex flex-col justify-between order-1 sm:order-2 col-span-1">
+						<p className=" mt-5 px-2 text-xl md:text-[28px] 0.5xl:text-3xl 3xl:text-[36px] font-DynaPuff font-medium">
+							{nombreProducto}
+						</p>
+					</div>
+					<div className=" mt-5 px-2 text-sm md:text-xl flex flex-col justify-between w-full order-3">
+						<div className="flex justify-between w-full">
+							<p className=" md:text-md font-DynaPuff font-medium">
+								Precio normal
+							</p>
+							<p className=" md:text-md font-DynaPuff font-medium">Oferta</p>
+						</div>
+						<div className=" mt-2 flex flex-col justify-between">
+							<div className=" flex justify-between w-full ">
+								<p className=" font-DynaPuff font-extralight ">
+									{formattedAmount(precioVenta)}
+								</p>
+								<p className=" font-DynaPuff ">
+									{formattedAmount(precioOferta)}
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="w-full h-10 my-5 order-4 md:col-span-1 md:col-start-2 0.5xl:pl-20 3xl:pl-64  rounded-md">
+						<button className=" w-full h-full bg-cyan-500 rounded-md font-DynaPuff md:text-md font-medium">
+							Consultar producto
+						</button>
+					</div>
+				</div>
+				<div className="w-full mt-5 flex flex-col lg:flex-row lg:gap-2">
+					<div className=" mt-5 px-2 w-full border border-slate-100 drop-shadow-md">
+						<h1 className="mt-5 font-DynaPuff font-medium md:text-[22px] ">
+							Descripción del producto
+						</h1>
+						<p className=" my-5 font-DynaPuff text-xs md:text-[16px]">
+							{descripcionProducto}
+						</p>
+					</div>
+					<div className="mt-5 px-2 w-full border border-slate-100 drop-shadow-md">
+						<h1 className="mt-5 font-DynaPuff font-medium md:text-[22px]">
+							Especificaciones del producto
+						</h1>
+						<p className=" my-5 font-DynaPuff text-xs md:text-[16px]">
+							{descripcionProducto}
+						</p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export { ProductCard };
