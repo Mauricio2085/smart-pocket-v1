0722c29a2cf367ce06cf5cc3f805b3643c135800
Mauricio2085 <maoca2085@gmail.com>
2025-02-14 16:05:31 -0500

    Corrección en la importación del componente SVG

--- src/components/FooterImage.jsx
+++ src/components/FooterImage.jsx
@@ -1,4 +1,4 @@
-import { FooterLogo } from "../assets/Smart_Pocket_V2_Logo_Web_200x40px.svg";
+import FooterLogo from "../assets/Smart_Pocket_V2_Logo_Web_200x40px.svg";
 
 const FooterImage = () => {
 	return <img src={FooterLogo} alt="Smart Pocket Logo" />;
