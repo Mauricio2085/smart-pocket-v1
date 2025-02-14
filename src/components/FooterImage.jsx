6b1534a7f97693c058adcac8ef9713e5c7fab9c3
Mauricio2085 <maoca2085@gmail.com>
2025-02-13 20:32:35 -0500

    Se ajusta el ancho del contenedor en el componente FooterImage y se reemplaza el componente FooterImage por SmartLogo en SmartIcon.

--- src/components/FooterImage.jsx
+++ src/components/FooterImage.jsx
@@ -2,7 +2,7 @@ import { ReactComponent as FooterLogo } from "../assets/Smart_Pocket_V2_Logo_Web
 
 const FooterImage = () => {
 	return (
-		<div className="md:w-[12rem] lg:w-72 flex justify-center items-center lg:items-start">
+		<div className="w-72 flex justify-center items-center lg:items-start">
 			<FooterLogo />
 		</div>
 	);
