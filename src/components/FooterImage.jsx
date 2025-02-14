e3d06c8c548ad9393f523e078c899c14561ecfb6
Mauricio2085 <maoca2085@gmail.com>
2025-02-14 16:09:11 -0500

    Reversión de cambios realizados en FooterImage

--- src/components/FooterImage.jsx
+++ src/components/FooterImage.jsx
@@ -1,7 +1,11 @@
-import FooterLogo from "../assets/Smart_Pocket_V2_Logo_Web_200x40px.svg";
+import { ReactComponent as FooterLogo } from "../assets/Smart_Pocket_V2_Logo_Web_200x40px.svg";
 
 const FooterImage = () => {
-	return <img src={FooterLogo} alt="Smart Pocket Logo" />;
+	return (
+		<>
+			<FooterLogo />
+		</>
+	);
 };
 
 export { FooterImage };
