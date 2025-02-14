58ee5d1d1805a1fd5d662de97c5418aa692408c0
Mauricio2085 <maoca2085@gmail.com>
2025-02-14 16:02:17 -0500

    Simplifica el componente FooterImage al reemplazar el uso de un componente SVG por una etiqueta img para mejorar la carga y la estructura.

--- src/components/FooterImage.jsx
+++ src/components/FooterImage.jsx
@@ -1,11 +1,7 @@
-import { ReactComponent as FooterLogo } from "../assets/Smart_Pocket_V2_Logo_Web_200x40px.svg";
+import { FooterLogo } from "../assets/Smart_Pocket_V2_Logo_Web_200x40px.svg";
 
 const FooterImage = () => {
-	return (
-		<>
-			<FooterLogo />
-		</>
-	);
+	return <img src={FooterLogo} alt="Smart Pocket Logo" />;
 };
 
 export { FooterImage };
