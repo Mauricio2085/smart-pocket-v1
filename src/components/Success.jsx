47d4157f18b984c2b9c3f9795b0aa218320e5204
Mauricio2085 <maoca2085@gmail.com>
2025-03-11 10:34:46 -0500

    Se añade componente Success. Se modifican estilos en Error y CreateForm

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,17 +1,17 @@
 import { useNavigate } from "react-router-dom";
 
-const Error = ({ message, setErrorMessage }) => {
+const Success = ({ message, setSuccessMessage }) => {
 	const navigate = useNavigate();
 	return (
 		<section className="w-full h-full text-base md:text-xl lg:text-3xl absolute top-0 left-0 flex bg-slate-500 opacity-90 z-10">
 			<div className="w-full p-20 flex flex-col font-DynaPuff bg-cyan-100">
-				<h1 className="text-red-500 justify-center items-center mx-auto mb-10 font-medium">
+				<h1 className="text-green-500 justify-center items-center mx-auto mb-10 font-medium">
 					{message}
 				</h1>
 				<button
 					className="border-yellow-100 border rounded-md bg-gradient-to-l from-cyan-500 to-blue-500 justify-center items-center w-[95px] h-8 mx-auto lg:w-[150px] lg:h-14"
 					onClick={() => {
-						setErrorMessage(false);
+						setSuccessMessage("");
 						navigate("./");
 					}}
 				>
@@ -22,4 +22,4 @@ const Error = ({ message, setErrorMessage }) => {
 	);
 };
 
-export { Error };
+export { Success };
